6b77540c9ac174d9dbe5dd9ffe9c99ab8ce11b84
fgraciani <>
2021-04-19 14:22:53 +0000

    test

--- airm2html.py
+++ airm2html.py
@@ -227,7 +227,80 @@ def create_contextual_model_terms_index_page():
   f.close() 
   
 def create_contextual_model_terms_with_supplements_index_page():
-  pass
+  from bs4 import BeautifulSoup
+  import airm
+  airm = airm.Airm()
+  airm_terms = airm.contextual_terms.to_dict('records')
+  html = open("docs/airm/templates/viewer/contextual-model-terms-with-supplements-template.html").read()
+  soup = BeautifulSoup(html, "lxml")
+  for record in airm_terms:
+    if record["supplement"] == "\t\t\t":
+      tr = soup.new_tag("tr")
+      td_supplement = soup.new_tag("td")
+      tr.insert(1,td_supplement)
+
+      td_ic_name = soup.new_tag("td")
+      td_ic_name["data-order"] = record["class name"]
+      filename = str(record['class name'])+".html"
+      filename = filename.replace("/", "-")
+      filename = filename.replace("*", "-")
+      filename = filename.replace(" ", "")
+      filename = filename.replace("\t", "")
+      filename = filename.replace("\n", "")
+      url = "contextual-model/"+filename
+      text = record["class name"]
+      print(text)
+      new_link = soup.new_tag("a")
+      new_link['href'] = url
+      new_link['target'] = "_blank"
+      new_link.string = text
+      td_ic_name.insert(1,new_link)
+      tr.insert(2,td_ic_name)
+      
+      if record["definition"] != "":
+        td_def = soup.new_tag("td")
+        td_def.string = str(record["definition"])
+        tr.insert(3,td_def)
+     
+      soup.find('tbody').insert(1,tr)
+    elif record["supplement"] == "\t\t\tEuropean Supplement":
+      tr = soup.new_tag("tr")
+      td_supplement = soup.new_tag("td")
+      span_supplement = soup.new_tag("spam")
+      span_supplement['class'] = "badge badge-secondary"
+      span_supplement.string = "European Supplement"
+      td_supplement.insert(1,span_supplement)
+      tr.insert(1,td_supplement)
+      
+      td_ic_name = soup.new_tag("td")
+      td_ic_name["data-order"] = record["class name"]
+      filename = str(record['class name'])+".html"
+      filename = filename.replace("/", "-")
+      filename = filename.replace("*", "-")
+      filename = filename.replace(" ", "")
+      filename = filename.replace("\t", "")
+      filename = filename.replace("\n", "")
+      url = "contextual-model/european-supplement/"+filename
+      text = record["class name"]
+      print(text)
+      new_link = soup.new_tag("a")
+      new_link['href'] = url
+      new_link['target'] = "_blank"
+      new_link.string = text
+      td_ic_name.insert(1,new_link)
+      tr.insert(2,td_ic_name)
+      
+      if record["definition"] != "":
+        td_def = soup.new_tag("td")
+        td_def.string = str(record["definition"])
+        tr.insert(3,td_def)
+     
+      soup.find('tbody').insert(1,tr)
+
+  f= open("docs/airm/viewer/1.0.0/contextual-model-terms-with-supplements.html","w+")
+  f.write(soup.prettify())
+  f.close()
+
 def create_contextual_model_terms_item_pages():
   pass
 
